3f6022c833350f86694817be1de15e8684156751
Jhutatip <zall096314@gmail.com>
2022-08-27 08:32:30 +0000

    c

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  return res.send('<h1>สวัสดีนามิอ้วน</h1>')
+  return res.send('<h1>นามิอ้วนสวยก็ไม่สวยเเถมอ้วนอีก</h1>')
 })
 
 const PORT = process.env.PORT || 3000
